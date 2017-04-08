615e2ae96f637d1764cd7e242d765179eab26a08
Polianin Kirill <k.polianin@gmail.com>
2017-04-09 02:59:14 +0300

    First commit

---
+++ src/main/java/fi/haagahelia/course/GamelistApplication.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistApplication.class, args);
+	}
+}
