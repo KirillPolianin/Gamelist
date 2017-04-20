de426d81bf3af11443fe5174113cdf6270bbec53
Polianin Kirill <k.polianin@gmail.com>
2017-04-20 12:19:27 +0300

    Added test classes for User and Game controllers

--- src/test/java/fi/haagahelia/course/GamelistApplicationTests.java
+++ src/test/java/fi/haagahelia/course/GamelistApplicationTests.java
@@ -1,16 +1,33 @@
 package fi.haagahelia.course;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.haagahelia.course.web.GameController;
+import fi.haagahelia.course.web.UserController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GamelistApplicationTests {
 
+	@Autowired
+	private GameController gameController;
+	
+	@Autowired
+	private UserController userController;
+	
 	@Test
-	public void contextLoads() {
+	public void gameContextLoads() throws Exception {
+		assertThat(gameController).isNotNull();
 	}
-
+	
+	@Test
+	public void userContextLoads() throws Exception {
+		assertThat(userController).isNotNull();
+	}
 }
